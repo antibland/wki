4d9007ee838a31aa6944e97b9bdb054ed37b06f6
Andy Hoffman <antibland@gmail.com>
2015-06-29 11:35:01 -0400

    Add basic routing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,42 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'White Knight Implants' });
+  res.render('index', {
+    section: 'index',
+    title: 'White Knight Implants'
+  });
+});
+
+/* GET lab page */
+router.get('/lab', function(req, res, next) {
+  res.render('lab', {
+    section: 'lab',
+    title: 'Our lab'
+  });
+});
+
+/* GET lab page */
+router.get('/work', function(req, res, next) {
+  res.render('work', {
+    section: 'work',
+    title: 'Our work'
+  });
+});
+
+/* GET lab page */
+router.get('/literature', function(req, res, next) {
+  res.render('literature', {
+    section: 'literature',
+    title: 'Literature'
+  });
+});
+
+/* GET lab page */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', {
+    section: 'contact',
+    title: 'Contact us'
+  });
 });
 
 module.exports = router;
